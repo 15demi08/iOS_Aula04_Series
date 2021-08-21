25f06596b05814e8df9c1929603565814b2b1ccc
Demétrius Jr <15demi08@users.noreply.github.com>
2021-08-21 11:27:16 -0300

    Initial Commit

---
+++ Aula04_AppCompras/Aula04_AppComprasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aula04_AppComprasApp.swift
+//  Aula04_AppCompras
+//
+//  Created by IOS SENAC on 21/08/21.
+//
+
+import SwiftUI
+
+@main
+struct Aula04_AppComprasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
